6c4de61871ef9a56d754ba90324d82145ef0678f
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2016-11-06 15:22:43 +0100

    README.rst => readme.html

--- scripts/compile-static.sh
+++ scripts/compile-static.sh
@@ -4,3 +4,6 @@ echo "Compiling..."
 rm -rf out/* out/**/* || exit 0
 mkdir -p out
 cp -r static/* out
+
+echo "Compiling README.rst..."
+rst2html5 --bootstrap-css --pretty-print-code --jquery --embed-content README.rst > out/readme.html
